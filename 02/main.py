20ee97bbee72b37592948a8a9bb4545e7b48b9ab
nosnow035 <northno0104@gmail.com>
2025-10-29 09:22:25 +0900

    message

---
+++ 02/main.py
@@ -0,0 +1,13 @@
+
+
+
+class Testclass:
+
+    def __int__(self,num):
+        self.num - num
+
+    def show_num(self):
+        print("num:", self.num)
+
+    if __name__-- "__main":
+        test_1 = Testclass
